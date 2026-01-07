384f43fdef03ed0831c31c9c168c32d62bf9bdf9
ml-trigvent <manohar@trigvent.com>
2026-01-07 18:02:59 +0530

    create a model and schema

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 const express = require("express");
-const connectDB =require("./config/database")
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 
-
-connectDB().then(() => {
-  console.log(" Database connection established")
-  app.listen(3000, () => {
-    console.log("server running on 3000 port");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Manu",
+    lastName: "Thakur",
+    email: "manu@trigvent.com",
+    password: "Manohar@123",
   });
-  
-}).catch((err)=> {
-  console.log("Database is not established")
+  try {
+    await user.save();
+    res.send("user Added successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
+connectDB()
+  .then(() => {
+    console.log(" Database connection established");
+    app.listen(3000, () => {
+      console.log("server running on 3000 port");
+    });
+  })
+  .catch((err) => {
+    console.log("Database is not established");
+  });
